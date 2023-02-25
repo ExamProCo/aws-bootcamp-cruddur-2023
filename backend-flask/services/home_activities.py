1c64f6c626282e6d2556a3fa5e5b6e474ac0b18a
Franklin Moux <fmremix@gmail.com>
2023-02-25 21:46:18 +0000

    created new notifications backend enpoint

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -22,5 +22,19 @@ class HomeActivities:
         'created_at': (now - timedelta(days=2)).isoformat()
       }],
     },
-    ]
-    return results
+     {
+      'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
+      'handle':  'worf',
+      'message': 'i am out of prune juice',
+        'created_at': (now - timedelta(days=7)).isoformat(),
+      'expires_at': (now + timedelta(days=9)).isoformat(),
+      'likes': 0,
+      'replies': []
+    },
+    {
+      'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+      'handle':  'garek',
+      'message': 'my dear doctor, i am just simple tailor',
+       'created_at': (now - timedelta(hours=1)).isoformat(),
+      'expires_at': (now + timedelta(hours=12)).isoformat(),
+      'likes': 0,
