cca9091eba5c25582658bd3f678ecc9c54af161f
Franklin Moux <fmremix@gmail.com>
2023-03-06 23:43:00 +0000

    subsegments xray

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -2,33 +2,28 @@ from datetime import datetime, timedelta, timezone
 from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
-    # xray ---
-    segment = xray_recorder.begin_segment('user_activities')
     try:
-      model = {
-        'errors': None,
-        'data': None
-      }
       model = {
         'errors': None,
         'data': None
       }
 
       now = datetime.now(timezone.utc).astimezone()
-
+      
       if user_handle == None or len(user_handle) < 1:
         model['errors'] = ['blank_user_handle']
       else:
         now = datetime.now()
-      results = [{
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
-        'created_at': (now - timedelta(days=1)).isoformat(),
-        'expires_at': (now + timedelta(days=31)).isoformat()
-      }]
-      model['data'] = results
+        results = [{
+          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+          'handle':  'Andrew Brown',
+          'message': 'Cloud is fun!',
+          'created_at': (now - timedelta(days=1)).isoformat(),
+          'expires_at': (now + timedelta(days=31)).isoformat()
+        }]
+        model['data'] = results
 
+# subsegments xray
       subsegment = xray_recorder.begin_subsegment('mock-data')
       # xray ---
       dict = {
@@ -40,3 +35,4 @@ class UserActivities:
     finally:  
     #  # Close the segment
       xray_recorder.end_subsegment()
+    return model
