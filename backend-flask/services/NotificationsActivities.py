c504487802d04435c70041d5b767c8b7572957f5
Franklin Moux <fmremix@gmail.com>
2023-03-09 02:50:07 +0000

    notif act

--- backend-flask/services/NotificationsActivities.py
+++ backend-flask/services/NotificationsActivities.py
@@ -4,8 +4,8 @@ class NotificationsActivities:
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Heavenly',
-      'message': 'Cloud is the way!',
+      'handle':  'coco',
+      'message': 'I am white unicorn',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
@@ -14,8 +14,8 @@ class NotificationsActivities:
       'replies': [{
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Worf',
-        'message': 'This post has no honor!',
+        'handle':  'worf',
+        'message': 'this post has no honor!',
         'likes_count': 0,
         'replies_count': 0,
         'reposts_count': 0,
