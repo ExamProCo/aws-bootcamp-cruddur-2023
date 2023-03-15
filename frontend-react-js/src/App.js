982c09a7de0af3cd9feb8c16c1b5900f3d7fb0ba
Papa Moussa FALL <papemfall@gmail.com>
2023-03-15 22:05:26 +0000

    update amplify configuration

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -20,7 +20,6 @@ import { Amplify } from 'aws-amplify';
 
 Amplify.configure({
   "AWS_PROJECT_REGION": process.env.REACT_APP_AWS_PROJECT_REGION,
-  "aws_cognito_identity_pool_id": process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
   "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
   "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
   "aws_user_pools_web_client_id": process.env.REACT_APP_CLIENT_ID,
