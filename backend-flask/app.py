9aea92015f28161211a98324b9bdd87e373e5618
Papa Moussa FALL <papemfall@gmail.com>
2023-03-15 13:07:03 +0000

    updates intrument AWS Xray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -98,11 +98,13 @@ def data_create_message():
   return
 
 @app.route("/api/activities/home", methods=['GET'])
+@xray_recorder.capture('activities_home')
 def data_home():
   data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
+@xray_recorder.capture('activities_notifications')
 def data_notifications():
   data = NotificationsActivities.run()
   return data, 200
