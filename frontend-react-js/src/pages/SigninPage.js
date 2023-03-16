e1b8cc1d85fa5078374948cac49c907721a608c6
Papa Moussa FALL <papemfall@gmail.com>
2023-03-16 19:21:16 +0000

    Implement Custom Signin, Signup and Confirmation Pages

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -21,6 +21,7 @@ export default function SigninPage() {
     try {
       Auth.signIn(email, password)
         .then(user => {
+          console.log('user',user)
           localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
           window.location.href = "/"
         })
