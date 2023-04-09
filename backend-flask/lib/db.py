aff7344bfcb27092fd1b8cebe935618adc08ba95
Papa Moussa FALL <papemfall@gmail.com>
2023-04-09 18:32:09 +0000

    Implement Schema Load, Seed, Scan Scripts and Pattern Scripts for Read and List Conversations

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -23,7 +23,7 @@ class Db:
     return template_content
 
   def init_pool(self):
-    connection_url = os.getenv("PROD_CONNECTION_URL")
+    connection_url = os.getenv("CONNECTION_URL")
     self.pool = ConnectionPool(connection_url)
   # when we want to commit data such as an insert
   # be sure to check for RETURNING in all uppercases
@@ -53,11 +53,11 @@ class Db:
       self.print_sql_err(error)
       #conn.rollback
 
-  def print_sql(self,title,sql):
+  def print_sql(self,title,sql,params={}):
     cyan = '\033[96m'
     no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
-    print(sql)
+    print(sql,params)
 
   def print_params(self,params):
     blue = '\033[94m'
@@ -65,7 +65,15 @@ class Db:
     print(f'{blue} SQL Params:{no_color}')
     for key, value in params.items():
       print(key, ":", value)
-  
+
+  def query_value(self,sql,params={}):
+    self.print_sql('value',sql,params)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(sql,params)
+        json = cur.fetchone()
+        return json[0]
+
   def query_wrap_object(self,template):
     sql = f"""
     (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
@@ -84,7 +92,7 @@ class Db:
 
   #When we want to return a json object
   def query_array_json(self, sql, params={}):
-    self.print_sql('array',sql)
+    self.print_sql('array',sql,params)
 
     wrapped_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
@@ -98,7 +106,7 @@ class Db:
 
   #When we want to return an array of json objects
   def query_object_json(self,sql,params={}):
-    self.print_sql('json',sql)
+    self.print_sql('json',sql,params)
     self.print_params(params)
     wrapped_sql = self.query_wrap_object(sql)
     with self.pool.connection() as conn:
