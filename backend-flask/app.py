34a49741504c73fde4a7a3d11a35584b5d113750
Soumyadip Bijuli <rust_bondcr007@yahoo.com>
2023-07-23 12:39:06 +0000

    honeycomb changes in app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -28,6 +28,12 @@ provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
+# Show this in the logs within the backend-flask app (STDOUT)
+simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(simple_processor)
+
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
 
 
 app = Flask(__name__)
