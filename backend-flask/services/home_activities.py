bc4c43c474d77fc67f109402e4c57ae88c2e931f
Soumyadip Bijuli <rust_bondcr007@yahoo.com>
2023-07-25 17:29:05 +0000

    fixing JWT

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -51,5 +51,17 @@ class HomeActivities:
       }
       ]
 
+      if cognito_user_id != None:
+        extra_crud = {
+          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+          'handle':  'Lore',
+          'message': 'My dear brother, it the humans that are the problem',
+          'created_at': (now - timedelta(hours=1)).isoformat(),
+          'expires_at': (now + timedelta(hours=12)).isoformat(),
+          'likes': 1042,
+          'replies': []
+        }
+        results.insert(0,extra_crud)
+
       span.set_attribute("app.result_length", len(results))
       return results
