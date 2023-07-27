e13d8e933bccbd9c9005fd906be3a6b9697e407e
Soumyadip Bijuli <rust_bondcr007@yahoo.com>
2023-07-27 17:09:33 +0000

    added lamba for post confirmation

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE public.users (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
   display_name text NOT NULL,
   handle text NOT NULL,
+  email text NOT NULL,
   cognito_user_id text NOT NULL,
   created_at TIMESTAMP default current_timestamp NOT NULL
 );
