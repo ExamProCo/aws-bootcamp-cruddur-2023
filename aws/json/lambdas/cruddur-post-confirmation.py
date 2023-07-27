ac62cad8d6e372240017f4add7f57d21296eb785
Soumyadip Bijuli <rust_bondcr007@yahoo.com>
2023-07-27 19:22:18 +0000

    fixed cruddur-post-confirmation.py

--- aws/json/lambdas/cruddur-post-confirmation.py
+++ aws/json/lambdas/cruddur-post-confirmation.py
@@ -10,7 +10,7 @@ def lambda_handler(event, context):
     user_display_name  = user['name']
     user_email         = user['email']
     user_handle        = user['preferred_username']
-    user_cognito_id    = user['sub']
+    cognito_user_id    = user['sub']
     try:
       print('entered-try')
       sql = f"""
@@ -20,22 +20,28 @@ def lambda_handler(event, context):
           handle, 
           cognito_user_id
           ) 
-        VALUES(%s,%s,%s,%s)
+        VALUES(
+          %(display_name)s,
+          %(email)s,
+          %(handle)s,
+          %(cognito_user_id)s
+        )
       """
       print('SQL Statement ----')
       print(sql)
       conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
       cur = conn.cursor()
-      params = [
-        user_display_name,
-        user_email,
-        user_handle,
-        user_cognito_id
-      ]
-      cur.execute(sql,*params)
+      params = {
+        'display_name': user_display_name,
+        'email': user_email,
+        'handle': user_handle,
+        'cognito_user_id': cognito_user_id
+      }
+      cur.execute(sql,params)
       conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
+      print('error:')
       print(error)
     finally:
       if conn is not None:
